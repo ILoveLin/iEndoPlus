3b6d9f77d01c134fc2d8e291a89f8b0023640d74
tongzhengtong <tongzhengtong@126.com>
2021-11-03 08:55:34 +0800

    update
    update

---
+++ app/src/main/java/com/company/iendo/mineui/activity/login/service/ServiceConfigActivity.java
@@ -0,0 +1,27 @@
+package com.company.iendo.mineui.activity.login.service;
+
+import com.company.iendo.R;
+import com.company.iendo.app.AppActivity;
+
+/**
+ * company：江西神州医疗设备有限公司
+ * author： LoveLin
+ * time：2021/11/1 15:59
+ * desc：服务器配置界面
+ */
+public class ServiceConfigActivity extends AppActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_service_config;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
