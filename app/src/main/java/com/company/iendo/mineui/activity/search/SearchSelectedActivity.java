65e59d8afdcf8cf13c896952040e228f7b8f1715
tongzhengtong <tongzhengtong@126.com>
2022-01-11 13:56:19 +0800

    update
    update

--- app/src/main/java/com/company/iendo/mineui/activity/search/SearchSelectedActivity.java
+++ app/src/main/java/com/company/iendo/mineui/activity/search/SearchSelectedActivity.java
@@ -1,5 +1,6 @@
 package com.company.iendo.mineui.activity.search;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.view.View;
 import android.widget.EditText;
@@ -7,7 +8,11 @@ import android.widget.TextView;
 
 import com.company.iendo.R;
 import com.company.iendo.app.AppActivity;
+import com.company.iendo.bean.DialogItemBean;
+import com.company.iendo.bean.ListDialogDateBean;
+import com.company.iendo.other.HttpConstant;
 import com.company.iendo.ui.dialog.DateDialog;
+import com.company.iendo.ui.dialog.MenuDialog;
 import com.company.iendo.ui.dialog.SelectModifyTypeDialog;
 import com.company.iendo.utils.DateUtil;
 import com.company.iendo.utils.LogUtils;
@@ -16,12 +21,18 @@ import com.hjq.bar.TitleBar;
 import com.hjq.base.BaseDialog;
 import com.hjq.shape.view.ShapeButton;
 import com.hjq.shape.view.ShapeTextView;
+import com.hjq.widget.view.ClearEditText;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
 
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.List;
+
+import okhttp3.Call;
 
 /**
  * company：江西神州医疗设备有限公司
@@ -33,7 +44,8 @@ import java.util.HashMap;
  * 性别默认给---全部
  */
 public class SearchSelectedActivity extends AppActivity {
-
+    private boolean mFragClickable = false;  //dialog数据请求错误,相对于dialog不允许弹窗,不然会闪退
+    private HashMap mDialogItemMap;
     private ShapeTextView mStartDate;
     private ShapeTextView mEndDate;
     private EditText mEtCheckNum;
@@ -79,7 +91,8 @@ public class SearchSelectedActivity extends AppActivity {
         mEtGetDoctor = findViewById(R.id.edit_get_doctor);  //送检医生
         mSearch = findViewById(R.id.sp_search);
         mTitleBar = findViewById(R.id.titlebar);
-        setOnClickListener(R.id.tv_sex_type, R.id.edit_worker, R.id.sp_search, R.id.sp_start_date, R.id.sp_end_date, R.id.tv_married_type);
+        setOnClickListener(R.id.tv_sex_type, R.id.edit_worker, R.id.edit_section, R.id.edit_device, R.id.edit_check_doctor,
+                R.id.edit_get_doctor, R.id.sp_search, R.id.sp_start_date, R.id.sp_end_date, R.id.tv_married_type);
 
         mTitleBar.setOnTitleBarListener(new OnTitleBarListener() {
             @Override
@@ -98,25 +111,141 @@ public class SearchSelectedActivity extends AppActivity {
 
             }
         });
+
+        sendListDictsRequest();
+    }
+
+    /**
+     * 获取需要Dialog选择数据的集合
+     */
+    private void sendListDictsRequest() {
+        //获取Dialog item的数据
+        OkHttpUtils.get()
+                .url(mBaseUrl + HttpConstant.CaseManager_CaseDialogDate)
+//                .addParams("EndoType", "4")
+                .addParams("EndoType", endoType)
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        LogUtils.e("edit====" + e);
+                        mFragClickable = false;
+                    }
+
+                    @SuppressLint("NewApi")
+                    @Override
+                    public void onResponse(String response, int id) {
+                        LogUtils.e("edit==onResponse==" + response);
+                        ListDialogDateBean mBean = mGson.fromJson(response, ListDialogDateBean.class);
+                        //1,按'DictName'进行分组
+                        //2,按照分组中找出 'ParentId=0' 的项,作为每个分组的类别
+                        List<ListDialogDateBean.DataDTO.ListDictsDTO> listDicts = mBean.getData().getListDicts();
+                        //创建一个map  key是 DictName,value是list
+                        mDialogItemMap = new HashMap<String, ArrayList<DialogItemBean>>();
+                        for (int i = 0; i < listDicts.size(); i++) {
+                            //获取每条数据的dictname
+                            String currentDictName = listDicts.get(i).getDictName();  //100
+                            //再次遍历这个集合,和currentDictName  相同的bean全部存入集合中
+                            ArrayList<DialogItemBean> itemBeanList = new ArrayList<>();
+                            for (int j = 0; j < listDicts.size(); j++) {
+                                ListDialogDateBean.DataDTO.ListDictsDTO listDictsDTO = listDicts.get(j);
+                                String dictName = listDictsDTO.getDictName();
+                                if (currentDictName.equals(dictName)) {
+                                    DialogItemBean itemBean = new DialogItemBean();
+                                    itemBean.setID(listDictsDTO.getID());
+                                    itemBean.setParentId(listDictsDTO.getParentId());
+                                    itemBean.setDictItem(listDictsDTO.getDictItem());
+                                    itemBean.setEndoType(listDictsDTO.getEndoType());
+                                    itemBeanList.add(itemBean);
+                                }
+                            }
+                            if (!itemBeanList.isEmpty()) {
+                                boolean currentDictName1 = mDialogItemMap.containsKey(currentDictName);
+                                if (!currentDictName1) {
+                                    mDialogItemMap.put(currentDictName, itemBeanList);
+                                }
+                            }
+                            mFragClickable = true;
+
+                        }
+
+//                        Iterator<Map.Entry<String, ArrayList<DialogItemBean>>> entries = mDialogItemMap.entrySet().iterator();
+//                        while (entries.hasNext()) {
+//                            Map.Entry<String, ArrayList<DialogItemBean>> entry = entries.next();
+//                            String key = entry.getKey();
+//                            ArrayList<DialogItemBean> value = entry.getValue();
+//                            LogUtils.e("对话框数据====key====" + key);
+//
+//                            for (int i = 0; i < value.size(); i++) {
+//                                DialogItemBean bean = value.get(i);
+//                                LogUtils.e("对话框数据====value====" + bean.getDictItem());
+//                            }
+//                        }
+                    }
+                });
+
+    }
+
+
+    private void showMenuDialog(EditText mEdit, String key) {
+        if (mFragClickable && null != mDialogItemMap) {
+            ArrayList<DialogItemBean> mDataList = (ArrayList<DialogItemBean>) mDialogItemMap.get(key);
+
+            ArrayList<String> stringList = new ArrayList<>();
+            for (int i = 0; i < mDataList.size(); i++) {
+                stringList.add(mDataList.get(i).getDictItem());
+            }
+            // 底部选择框
+            new MenuDialog.Builder(this)
+                    // 设置 null 表示不显示取消按钮
+                    //.setCancel(getString(R.string.common_cancel))
+                    // 设置点击按钮后不关闭对话框
+                    //.setAutoDismiss(false)
+                    .setList(stringList)
+                    .setListener(new MenuDialog.OnListener<String>() {
+
+                        @Override
+                        public void onSelected(BaseDialog dialog, int position, String data) {
+                            String s = stringList.get(position);
+                            LogUtils.e("MenuDialog====位置：" + position + "，文本：" + data);
+                            LogUtils.e("MenuDialog===s==" + s); //{0=HD3}
+                            mEdit.setText(mEdit.getText().toString() + "" + s);
+
+                        }
+
+                        @Override
+                        public void onCancel(BaseDialog dialog) {
+                            toast("取消了");
+                        }
+                    })
+                    .show();
+        }
     }
 
     private void setDefaultDate() {
         String systemDate = DateUtil.getSystemDate();
         mStartDate.setText("" + systemDate);
         mEndDate.setText("" + systemDate);
-        mEtCheckNum.setHint("请输入检查号");
-        mEtCheckName.setHint("请输入姓名");
-        mEtEndAge.setHint("请输入年龄");
         mSexType.setText("请选择性别");
-        mEtWorker.setHint("请输入职业");
         mMarriedType.setText("请选择婚否");
-        mEtSection.setHint("请输入科室");
-        mEtDevice.setHint("请输入设备");
-        mEtCheckDoctor.setHint("请输入检查医生");
-        mEtGetDoctor.setHint("请输入送检医生");
-
-
+        mEtCheckNum.setText("");
+        mEtCheckNum.setHint("请输入检查号");
+        mEtCheckName.setText("");
+        mEtCheckName.setHint("请输入姓名");
+        mEtEndAge.setText("");
+        mEtEndAge.setHint("请输入年龄");
+        mEtWorker.setText("");
+        mEtWorker.setHint("请输入职业");
+        mEtSection.setText("");
+        mEtSection.setHint("请选择科室");
+        mEtDevice.setText("");
+        mEtDevice.setHint("请选择设备");
+        mEtCheckDoctor.setText("");
+        mEtCheckDoctor.setHint("请选择检查医生");
+        mEtGetDoctor.setText("");
+        mEtGetDoctor.setHint("请选择送检医生");
     }
+
 
     @Override
     protected void initData() {
@@ -133,12 +262,21 @@ public class SearchSelectedActivity extends AppActivity {
             case R.id.sp_end_date:  //结束时间
                 showDateDialog("结束");
                 break;
-//            case R.id.tv_age_type:  //年龄类别   type=1
-//                mAgeList.add("岁");
-//                mAgeList.add("月");
-//                mAgeList.add("天");
-//                showSelectTypeDialog(mAgeList, "1");
-//                break;
+            case R.id.edit_worker:  //职业
+                showMenuDialog(mEtWorker, "5");
+                break;
+            case R.id.edit_section:  //科室
+                showMenuDialog(mEtSection, "9");
+                break;
+            case R.id.edit_device:  //设备
+                showMenuDialog(mEtDevice, "10");
+                break;
+            case R.id.edit_check_doctor:  //检查医生
+                showMenuDialog(mEtCheckDoctor, "20");
+                break;
+            case R.id.edit_get_doctor:  //送检医生
+                showMenuDialog(mEtGetDoctor, "8");
+                break;
             case R.id.tv_sex_type:  //性别类别  type=2
                 mSexList.clear();
                 mSexList.add("全部");
